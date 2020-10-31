5dd66f39dc5cf167e6c1db65979de92d55415ca5
nirrattner <nirrattner@gmail.com>
2020-10-31 16:48:22 -0400

    Color differences and show best run

--- src/main/java/com/nirrattner/splittimer/views/splits/SplitsPane.java
+++ src/main/java/com/nirrattner/splittimer/views/splits/SplitsPane.java
@@ -21,7 +21,10 @@ import java.util.stream.Collectors;
 @Singleton
 public class SplitsPane extends ScrollPane implements SplitsStateListener {
 
-  private static final String LABEL_STYLE_CLASS = "split-label";
+  private static final String SPLIT_LABEL_STYLE_CLASS = "split-label";
+  private static final String BEST_SPLIT_LABEL_STYLE_CLASS = "best-split-label";
+  private static final String PLUS_SPLIT_LABEL_STYLE_CLASS = "plus-split-label";
+  private static final String MINUS_SPLIT_LABEL_STYLE_CLASS = "minus-split-label";
   private static final double LABEL_WIDTH = 150;
 
   private final TimeFormatter timeFormatter;
@@ -57,7 +60,7 @@ public class SplitsPane extends ScrollPane implements SplitsStateListener {
     HBox hBox = new HBox();
 
     Label nameLabel = new Label(split.getConfiguration().getName());
-    nameLabel.getStyleClass().add(LABEL_STYLE_CLASS);
+    nameLabel.getStyleClass().add(SPLIT_LABEL_STYLE_CLASS);
     hBox.getChildren().add(nameLabel);
 
     split.getTimestamp()
@@ -67,22 +70,31 @@ public class SplitsPane extends ScrollPane implements SplitsStateListener {
             .map(previousValue -> value - previousValue))
         .map(timeFormatter::formatSplitDifference)
         .map(Label::new)
-        .peek(label -> label.getStyleClass().add(LABEL_STYLE_CLASS))
+        .peek(label -> label.getStyleClass().add(
+            label.getText().startsWith("+")
+                ? PLUS_SPLIT_LABEL_STYLE_CLASS
+                : MINUS_SPLIT_LABEL_STYLE_CLASS))
+        .peek(label -> label.getStyleClass().add(SPLIT_LABEL_STYLE_CLASS))
         .map(StackPane::new)
         .peek(pane -> pane.setAlignment(Pos.BASELINE_RIGHT))
         .peek(pane -> HBox.setHgrow(pane, Priority.ALWAYS))
         .forEach(hBox.getChildren()::add);
 
     split.getTimestamp()
+        .or(split.getConfiguration()::getBestRunTimestamp)
         .stream()
         .map(timeFormatter::formatSplit)
         .map(Label::new)
-        .peek(label -> label.getStyleClass().add(LABEL_STYLE_CLASS))
+        .peek(label -> {
+          if (split.getTimestamp().isEmpty())
+            label.getStyleClass().add(BEST_SPLIT_LABEL_STYLE_CLASS);
+        })
+        .peek(label -> label.getStyleClass().add(SPLIT_LABEL_STYLE_CLASS))
         .map(StackPane::new)
         .peek(pane -> pane.setAlignment(Pos.BASELINE_RIGHT))
         .peek(pane -> HBox.setHgrow(pane, Priority.ALWAYS))
         .peek(pane -> {
-          if (split.getConfiguration().getBestRunTimestamp().isPresent() || split.getConfiguration().getBestTime().isPresent()) {
+          if (split.getConfiguration().getBestRunTimestamp().isPresent() && split.getTimestamp().isPresent()) {
             pane.setMinWidth(LABEL_WIDTH);
             pane.setMaxWidth(LABEL_WIDTH);
           }
